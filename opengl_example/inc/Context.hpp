a641a8b702d2ff055fabb1d706f73abcdd72a972
gunjoong-kim <rjsoownd0419@gmail.com>
2023-11-08 00:35:31 +0900

    Add some call mouse callback

--- opengl_example/inc/Context.hpp
+++ opengl_example/inc/Context.hpp
@@ -15,6 +15,12 @@ class Context
 public:
 	static ContextUPtr Create();
 	void Render();
+	void ProcessInput(GLFWwindow* window);
+	void Reshape(int width, int height);
+	void MouseMove(double x, double y);
+	void MouseButton(int button, int action, double x, double y);
+
+	
 
 private:
 	Context() {}
@@ -26,6 +32,18 @@ private:
 	TextureUPtr m_texture;
 	TextureUPtr m_texture2;
 
+	glm::vec3 m_cameraPos { glm::vec3(0.0f, 0.0f, 3.0f) };
+	glm::vec3 m_cameraFront { glm::vec3(0.0f, 0.0f, -1.0f) };
+	glm::vec3 m_cameraUp { glm::vec3(0.0f, 1.0f, 0.0f) };
+
+	int m_width {WINDOW_WIDTH};
+	int m_height {WINDOW_HEIGHT};
+
+	float m_cameraPitch { 0.0f };
+	float m_cameraYaw { 0.0f };
+
+	bool m_cameraControl { false };
+	glm::vec2 m_prevMousePos { glm::vec2(0.0f) };
 };
 
 #endif // __CONTEXT_HPP__
--- opengl_example/src/main.cpp
+++ opengl_example/src/main.cpp
@@ -5,29 +5,43 @@
 
 void OnFramebufferSizeChange(GLFWwindow* window, int width, int height)
 {
-    SPDLOG_INFO("framebuffer size changed: ({} x {})", width, height);
-    glViewport(0, 0, width, height);
+	SPDLOG_INFO("framebuffer size changed: ({} x {})", width, height);
+	auto context = (Context*)glfwGetWindowUserPointer(window);
+	context->Reshape(width, height);
+}
+
+void OnCursorPos(GLFWwindow* window, double x, double y) {
+	SPDLOG_INFO("mouse location: ({} x {})", x, y);
+	auto context = (Context*)glfwGetWindowUserPointer(window);
+	context->MouseMove(x, y);
+}
+
+void OnMouseButton(GLFWwindow* window, int button, int action, int modifier) {
+	auto context = (Context*)glfwGetWindowUserPointer(window);
+	double x, y;
+	glfwGetCursorPos(window, &x, &y);
+	context->MouseButton(button, action, x, y);
 }
 
 void OnKeyEvent
 (
-    GLFWwindow* window,
-    int key,
-    int scancode,
-    int action,
-    int mods
+	GLFWwindow* window,
+	int key,
+	int scancode,
+	int action,
+	int mods
 ) {
-    SPDLOG_INFO("key: {}, scancode: {}, action: {}, mods: {}{}{}",
-        key, scancode,
-        action == GLFW_PRESS ? "Pressed" :
-        action == GLFW_RELEASE ? "Released" :
-        action == GLFW_REPEAT ? "Repeat" : "Unknown",
-        mods & GLFW_MOD_CONTROL ? "C" : "-",
-        mods & GLFW_MOD_SHIFT ? "S" : "-",
-        mods & GLFW_MOD_ALT ? "A" : "-");
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
-        glfwSetWindowShouldClose(window, true);
-    }
+	SPDLOG_INFO("key: {}, scancode: {}, action: {}, mods: {}{}{}",
+		key, scancode,
+		action == GLFW_PRESS ? "Pressed" :
+		action == GLFW_RELEASE ? "Released" :
+		action == GLFW_REPEAT ? "Repeat" : "Unknown",
+		mods & GLFW_MOD_CONTROL ? "C" : "-",
+		mods & GLFW_MOD_SHIFT ? "S" : "-",
+		mods & GLFW_MOD_ALT ? "A" : "-");
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
 }
 
 int main()
@@ -79,14 +93,19 @@ int main()
 		return -1;
 	}
 
+	glfwSetWindowUserPointer(window, context.get());//userpointer 기능 어떠한 포인터를 등록해놓을 수 있음
+
 	OnFramebufferSizeChange(window, WINDOW_WIDTH, WINDOW_HEIGHT);
 	glfwSetFramebufferSizeCallback(window, OnFramebufferSizeChange);
 	glfwSetKeyCallback(window, OnKeyEvent);
+	glfwSetCursorPosCallback(window, OnCursorPos);
+	glfwSetMouseButtonCallback(window, OnMouseButton);
 
 	// glfw 루프 실행, 윈도우 close 버튼을 누르면 정상 종료
     SPDLOG_INFO("Start main loop");
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
+		context->ProcessInput(window);
 		context->Render();
 		glfwSwapBuffers(window);
     }
